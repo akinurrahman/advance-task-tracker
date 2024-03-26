770e95f9d5b914efac6d155b4bf3a56f381261a9
Akinur Rahman <akinurrahmanofficial@gmail.com>
2024-03-26 06:54:20 +0530

    folder structure done

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { FaUserAlt } from "react-icons/fa";
+import TaskList from "./components/tasks/TaskList";
+import Filters from "./components/Filters";
+
+const App = () => {
+  return (
+    <div className="mx-auto max-w-[1200px]  p-6">
+      <div className="flex items-center justify-between">
+        <h2 className="text-2xl font-semibold">Task Board</h2>
+        <FaUserAlt className="rounded-full bg-white p-1 text-xl" size={35} />
+      </div>
+      <div className="mt-6 rounded-xl border-2 border-solid border-white p-6">
+        <Filters />
+        <TaskList />
+      </div>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/form/AddTaskForm.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const AddTaskForm = () => {
+  return <div></div>;
+};
+
+export default AddTaskForm;
